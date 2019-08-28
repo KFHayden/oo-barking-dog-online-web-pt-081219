2cdf888eb4f75f5b724c0a5a4b3fcaab41ee3649
Kevin Hayden <kevin.fhayden@gmail.com>
2019-08-28 22:45:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,24 @@
-# Your code goes here!
+class Dog
+  
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
+  
+  def bark=(dogs_bark)
+    @this_dogs_bark = dogs_bark
+  end
+  
+  def bark
+    @this_dogs_bark
+  end
+  
+  def name
+    @this_dogs_name
+  end
+end
+
+fido = Dog.new
+
+fido.bark = "woof!"
+
+puts fido.bark
